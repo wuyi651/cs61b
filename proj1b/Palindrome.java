9ae3dd60c1cfd3a61028c2b373ab2d5d715ff09e
wuyi651 <liuwuyi386@gmail.com>
2022-05-25 18:05:35 -0700

    complete proj1b

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,61 @@
+public class Palindrome {
+    public Deque<Character> wordToDeque(String word) {
+        Deque<Character> d = new LinkedListDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            d.addLast(word.charAt(i));
+        }
+        return d;
+    }
+
+    public boolean isPalindrome(String word) {
+//        if (word.length() == 0 || word == null) {
+//            return true;
+//        }
+//        int left = 0;
+//        int right = word.length() - 1;
+//        for (int i = 0; i <= word.length()/2; i++) {
+//            if (word.charAt(left) == word.charAt(right)) {
+//                left += 1;
+//                right -= 1;
+//            } else {
+//                return false;
+//            }
+//        }
+//        return true;
+        if (word == null || word.isEmpty()) {
+            return true;
+        }
+        Deque d = wordToDeque(word);
+        return isPalindromehelper(d);
+    }
+
+    public boolean isPalindromehelper(Deque word) {
+        if (word.size() <= 1) {
+            return true;
+        }
+        if (word.removeFirst() != word.removeLast()) {
+            return false;
+        }
+        return isPalindromehelper(word);
+    }
+
+    public boolean isPalindrome(String word, CharacterComparator cc) {
+        if (word == null || word.isEmpty()) {
+            return true;
+        }
+        Deque d = wordToDeque(word);
+        return isPalindromehelper2(d,cc);
+    }
+
+    public boolean isPalindromehelper2(Deque word,CharacterComparator cc) {
+        if (word.size() <= 1) {
+            return true;
+        }
+
+        if (!cc.equalChars((char)word.removeFirst(),(char)word.removeLast())) {
+            return false;
+        }
+        return isPalindromehelper2(word,cc);
+    }
+
+}
--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -2,9 +2,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
-    /*// You must use this palindrome, and not instantiate
+    // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
+    static CharacterComparator cc = new OffByOne();
 
     @Test
     public void testWordToDeque() {
@@ -14,5 +15,19 @@ public class TestPalindrome {
             actual += d.removeFirst();
         }
         assertEquals("persiflage", actual);
-    } Uncomment this class once you've created your Palindrome class. */
+    }
+
+    @Test
+    public void TestPalindrome() {
+        assertFalse(palindrome.isPalindrome("cat"));
+        assertTrue("Failed",palindrome.isPalindrome("a"));
+        assertTrue("Failed",palindrome.isPalindrome(""));
+        assertTrue("Failed",palindrome.isPalindrome("noon"));
+       // assertTrue("Failed",palindrome.isPalindrome("cat"));
+        assertTrue("Failed",palindrome.isPalindrome(null));
+        assertTrue("Failed",palindrome.isPalindrome("flake",cc));
+        assertTrue("Failed",palindrome.isPalindrome("f",cc));
+    }
+
+
 }
